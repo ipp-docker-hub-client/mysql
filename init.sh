d70a167d648d1adbefbe0ca6058e17977906a7fe
ipp-docker-hub-client <ipp-docker-hub-client@users.noreply.github.com>
2016-08-20 14:39:11 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eo pipefail
 
+# comment out a few problematic configuration values
+# don't reverse lookup hostnames, they are usually another container
+sed -Ei 's/^(bind-address|log)/#&/' /etc/mysql/my.cnf
+echo 'skip-host-cache\nskip-name-resolve' | awk '{ print } $1 == "[mysqld]" && c == 0 { c = 1; system("cat") }' /etc/mysql/my.cnf > /tmp/my.cnf
+mv /tmp/my.cnf /etc/mysql/my.cnf
+
 # if command starts with an option, prepend mysqld
 if [ "${1:0:1}" = '-' ]; then
 	set -- mysqld "$@"
